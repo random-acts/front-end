9983f77e80c002ef07cb399c885601b7a2a25af4
Carlossantana987 <carlossantana987@outlook.com>
2019-06-24 17:25:40 -0400

    pushing actions and reducers

--- your-app/src/Store.js
+++ your-app/src/Store.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { createStore, applyMidddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+
 import rootReducer from "./reducers";
-
-const initialState = {};
-
-const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
-  composeWithDevTools(applyMidddleware(...middleware))
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
---
+++ your-app/src/reducers/index.js
@@ -0,0 +1,34 @@
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from "../actions";
+
+const defaultState = {
+  logginIn: false,
+  error: null,
+  loading: true,
+  token: localStorage.getItem("token")
+};
+
+export const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        loggingIn: true,
+        error: false
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggingIn: false,
+        error: false,
+        token: localStorage.getItem("token")
+      };
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        loggingIn: false,
+        error: action.payload
+      };
+  }
+};
+
+export default reducer;
