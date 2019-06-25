13f39bcd8058424d6b15fa21ec641e4da33a52b1
Andrew Brudnak <brudnak@icloud.com>
2019-06-24 21:33:55 -0700

    Removed unneeded node module folder
    - Removed CSS imports from App and Index

--- your-app/src/App.js
+++ your-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './Store';
