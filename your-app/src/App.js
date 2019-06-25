49f0daef0d5643416066b8ed1a8495ad6107a9a2
Andrew Brudnak <brudnak@icloud.com>
2019-06-24 21:23:26 -0700

    Added in Global Style for Eric Meyer reset

--- your-app/src/App.js
+++ your-app/src/App.js
@@ -1,37 +1,25 @@
-
-import React from "react";
-import "./App.css";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import store from "./Store";
-import Login from "./components/Login";
-import Navigation from "./components/NavBar";
-import Home from "./components/Home";
-import PrivateRoute from "./components/Home";
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './Store';
 import Login from './components/Login';
-import Contacts from './components/contacts/Contacts';
-
+import Navigation from './components/NavBar';
+import Home from './components/Home';
+import PrivateRoute from './components/Home';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
     <Provider store={store}>
-
+      <GlobalStyle />
       <Router>
-        <div className="App">
+        <div className='App'>
           <Navigation />
-          <Route path="/login" component={Login} />
-          <PrivateRoute exact path="/protected" component={Home} />
+          <Route path='/login' component={Login} />
+          <PrivateRoute exact path='/protected' component={Home} />
         </div>
       </Router>
-
-      <div className='App'>
-        <Login />
-        <Contacts />
-      </div>
     </Provider>
   );
 }
